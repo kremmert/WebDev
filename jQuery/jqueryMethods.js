b98281ee46d856dcb89535b653d2fda7959e1255
Kyle Remmert <kremmert236@yahoo.com>
2018-05-18 10:40:47 -0700

    jquery effects

--- jQuery/jqueryMethods.js
+++ jQuery/jqueryMethods.js
@@ -24,3 +24,52 @@ $("h2").addClass("wrong");
 $("h1").removeClass("correct");
 
 $("li").toggleClass("done");
+
+$("button").click(function() {
+	alert("someone clicked a button!");
+	$(this).css("background-color", "green");
+	console.log("You clicked the button: " + $(this).text());
+});
+
+$("h1").click(function() {
+	alert("h1 clicked!");
+});
+
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13) {
+		console.log("hit enter");
+	}
+});
+
+$("h2").on("click", function() {
+	$(this).css("background-color", "yellow");
+});
+
+$("h2").on("dblclick", function() {
+	$(this).css("background-color", "purple");
+});
+
+$("input").on("keypress", function(event) {
+	console.log(event.which);
+});
+
+$(".test").on("click", function() {
+	$(this).css("background-color", "purple");
+});
+
+$(".test").on("mouseenter", function() {
+	$(this).css("background-color", "green");
+});
+
+$(".test").on("mouseleave", function() {
+	$(this).css("background-color", "white");
+});
+
+$("#fade").on("click", function() {
+	//there's also fadeIn, fadeToggle
+	//slideDown/slideUp/slideToggle changes height
+	$("div").fadeOut(1000, function() {
+		console.log("fade completed");
+	});
+});
+
